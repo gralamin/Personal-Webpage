7e380aa942098d69cb184d1a72ec95b7a00ad50c
Glen Nelson <gralamin@gralamin.com>
2013-06-03 03:07:19 -0600

    Make the warning only appear once

---
+++ docBegin.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+        <meta name="description" content="Test site">
+        <meta name="keywords" content="HTML, PHP, Test">
+        <title>Glen Nelson - Computer Scientist</title>
+        <link href="style/main.css" rel="stylesheet" type="text/css">
+        <link href="style/lightbox.css" rel="stylesheet" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Copse' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
